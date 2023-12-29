3a58c5f8b995ce80e1355d6f005cac3614b2bdc8
TROLOTHUB <elliotdlm3@gmail.com>
2023-12-29 02:20:50 -0500

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import NewPage from './newpage.js'; // Asegúrate de que el import sea correcto
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Bienvenido a la página de inicio</h1>
+      {/* Otros elementos de tu página de inicio van aquí */}
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Las Rutas determinarán qué componente se renderiza */}
+        <Routes>
+          <Route path="/newpage" element={<NewPage />} />
+          <Route path="/" element={<HomeWithHeader />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
+// Crear un componente que incluya el encabezado y el componente Home
+const HomeWithHeader = () => {
+  return (
+    <>
+      <header className="App-header">
+        <Link to="/newpage">
+          <img src="/logo.png" className="App-logo" alt="logo" />
+        </Link>
+      </header>
+      <Home />
+    </>
+  );
+};
 
 export default App;
---
+++ src/newpage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'; // Asegúrate de que esta importación esté si tus estilos están definidos aquí
+
+const NewPage = () => {
+  return (
+    <div className="new-page">
+      {}
+    </div>
+  );
+};
+
+export default NewPage;
